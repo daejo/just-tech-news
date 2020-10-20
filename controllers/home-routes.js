0561f266d09bf0bef6633da83cb262f4258b1b88
daejo <daejo@icloud.com>
2020-10-20 00:21:48 -0700

    adds login and sign up form submission

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-// SAYS WHICH TEMPLATE IS GOING TO BE USED
+
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: [
@@ -38,5 +38,9 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+  });
 
 module.exports = router;
