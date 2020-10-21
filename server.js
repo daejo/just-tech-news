40675a67707aa876433039fcf2db8521658ecbd5
daejo <daejo@icloud.com>
2020-10-21 03:32:09 -0700

    finalizes codes

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
